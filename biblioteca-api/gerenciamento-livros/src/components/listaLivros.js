701bac7c3dc5108f2ecee65a0c49551930992b62
Mateus Oliveira <oliveiramateus1222@gmail.com>
2024-08-29 21:54:45 -0300

    29/08/24

--- biblioteca-api/gerenciamento-livros/src/components/listaLivros.js
+++ biblioteca-api/gerenciamento-livros/src/components/listaLivros.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 
 function ListaLivros() {
   const [livros, setLivros] = useState([]);
+  const [pesquisa, setPesquisa] = useState("");
 
   useEffect(() => {
     axios
@@ -19,6 +20,16 @@ function ListaLivros() {
       .catch((error) => console.error("Erro ao deletar o livro:", error));
   };
 
+  const livrosFiltrados = livros.filter((livro) => {
+    const pesquisaLower = pesquisa.toLowerCase();
+    return (
+      livro.titulo.toLowerCase().includes(pesquisaLower) ||
+      livro.autor.toLowerCase().includes(pesquisaLower) ||
+      livro.genero.toLowerCase().includes(pesquisaLower) ||
+      livro.ano.toString().includes(pesquisaLower)
+    );
+  });
+
   return (
     <div>
       <h1>Lista de Livros</h1>
@@ -26,19 +37,32 @@ function ListaLivros() {
         Adicionar Novo Livro
       </Link>
 
+      <div className="pesquisa">
+        <input
+          type="text"
+          placeholder="Pesquisar por título, autor, gênero ou ano"
+          value={pesquisa}
+          onChange={(e) => setPesquisa(e.target.value)}
+        />
+      </div>
+
       <table>
         <thead>
           <tr>
             <th>Título</th>
             <th>Autor</th>
+            <th>Ano</th>
+            <th>Gênero</th>
             <th>Ações</th>
           </tr>
         </thead>
         <tbody>
-          {livros.map((livro) => (
+          {livrosFiltrados.map((livro) => (
             <tr key={livro._id}>
               <td>{livro.titulo}</td>
               <td>{livro.autor}</td>
+              <td>{livro.ano}</td>
+              <td>{livro.genero}</td>
               <td>
                 <Link to={`/editar/${livro._id}`}>
                   <button>Editar</button>
