122b70fb1d20c3ee9bf478920763a6fe57827dc8
Mateus Oliveira <oliveiramateus1222@gmail.com>
2024-09-03 20:08:42 -0300

    03/09/24

--- ProjetoToDoListAF/todo-list-simple/app/api/todos/[id]/route.js
+++ ProjetoToDoListAF/todo-list-simple/app/api/todos/[id]/route.js
@@ -4,30 +4,53 @@ import { NextResponse } from "next/server";
 
 export async function PUT(req, { params }) {
   await connectMongo();
+
   try {
     const data = await req.json();
     const todo = await Todo.findByIdAndUpdate(params.id, data, {
       new: true,
       runValidators: true,
     });
+
     if (!todo) {
-      return NextResponse.json({ success: false }, { status: 400 });
+      return NextResponse.json(
+        { success: false, message: "Tarefa não encontrada." },
+        { status: 404 }
+      );
     }
-    return NextResponse.json({ success: true, data: todo });
+
+    return NextResponse.json({ success: true, data: todo }, { status: 200 });
   } catch (error) {
-    return NextResponse.json({ success: false }, { status: 400 });
+    console.error("Erro ao atualizar a tarefa:", error);
+    return NextResponse.json(
+      { success: false, message: "Erro ao atualizar a tarefa." },
+      { status: 500 }
+    );
   }
 }
 
 export async function DELETE(req, { params }) {
   await connectMongo();
+
   try {
-    const deletedTodo = await Todo.deleteOne({ _id: params.id });
+    const deletedTodo = await Todo.findByIdAndDelete(params.id);
+
     if (!deletedTodo) {
-      return NextResponse.json({ success: false }, { status: 400 });
+      return NextResponse.json(
+        { success: false, message: "Tarefa não encontrada." },
+        { status: 404 }
+      );
     }
-    return NextResponse.json({ success: true, data: {} });
+
+    return NextResponse.json(
+      { success: true, message: "Tarefa excluída com sucesso." },
+      { status: 200 }
+    );
   } catch (error) {
-    return NextResponse.json({ success: false }, { status: 400 });
+    console.error("Erro ao excluir a tarefa:", error);
+    return NextResponse.json(
+      { success: false, message: "Erro ao excluir a tarefa." },
+      { status: 500 }
+    );
   }
 }
