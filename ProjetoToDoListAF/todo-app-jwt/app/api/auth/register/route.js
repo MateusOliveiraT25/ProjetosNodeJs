c209900bcf693c2d27278d7e802d7c1df5e6f82c
Mateus Oliveira <oliveiramateus1222@gmail.com>
2024-09-05 20:56:22 -0300

    05/09/24

--- ProjetoToDoListAF/todo-app-jwt/app/api/auth/register/route.js
+++ ProjetoToDoListAF/todo-app-jwt/app/api/auth/register/route.js
@@ -1,33 +1,15 @@
 import User from "@/models/User";
-import mongoose from "mongoose";
-import bcrypt from "bcrypt";
-import { NextResponse } from "next/server";
 import connectMongo from "@/utils/dbConnect";
+import { NextResponse } from "next/server";
 
 export async function POST(request) {
-  const { username, password } = await request.json();
-
+  const data = await request.json();
   await connectMongo();
 
-    // Hasheia a senha de forma assíncrona
-    const passwordHash = await bcrypt.hash(password, 10);
-
-    // Cria o usuário com a senha hasheada
-    const user = new User({
-      username,
-      password: passwordHash,
-    });
-
   try {
-    // Salva o usuário no banco de dados
-    await user.save();
-
-    return NextResponse.json({ success: true }, { status: 200 });
+    const user = await User.create(data);
+    return NextResponse.json({ success: true, data: user });
   } catch (error) {
-    console.error(error); // Loga o erro para depuração
-    return NextResponse.json(
-      { success: false, message: "Erro ao criar o usuário" },
-      { status: 400 }
-    );
+    return NextResponse.json({ success: false }, { status: 400 });
   }
 }
--- ProjetoToDoListAF/todo-app-jwt/models/User.js
+++ ProjetoToDoListAF/todo-app-jwt/models/User.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const UserSchema = new mongoose.Schema({
   username: {
@@ -12,5 +13,22 @@ const UserSchema = new mongoose.Schema({
   },
 });
 
+// Hash a senha antes de salvar
+UserSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+// Método para comparar senhas
+UserSchema.methods.comparePassword = function (candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+// Verificar se o modelo já foi criado para evitar erro de recompilação no Next.js
 const User = mongoose.models.User || mongoose.model("User", UserSchema);
+
 export default User;
