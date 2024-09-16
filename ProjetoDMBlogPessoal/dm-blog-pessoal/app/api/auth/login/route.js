9811e2d8fdd27e9320ca77d9780cdd77062eebc2
Mateus Oliveira <oliveiramateus1222@gmail.com>
2024-09-16 20:05:58 -0300

    16/09/24

--- ProjetoDMBlogPessoal/dm-blog-pessoal/app/api/auth/login/route.js
+++ ProjetoDMBlogPessoal/dm-blog-pessoal/app/api/auth/login/route.js
@@ -1,24 +1,26 @@
-import Usuario from "@/model/Usuario";
+import Usuario from "@//models/Usuario.js";
 import connectMongo from "@/utils/dbConnect";
 import { NextResponse } from "next/server";
+
 import jwt from "jsonwebtoken";
 
 export async function POST(request) {
-    const {usuarioname, password} = await request.json();
-    await connectMongo();
+  const { usuarioname, password } = await request.json();
+  await connectMongo();
 
-    //verificar se o usuario existe
-    try {
-        const usuario = await Usuario.findOne({username});
-        if (!usuario && !(await usuario.comparePassword(password))) {
-            return NextResponse.json({sucess:false}, {status:400})
-        }
-
-        //Criar Minha Token de Autorização
-        const token = jwt.sign({usuarioId: usuario._id},
-            process.env.JWT_SECRET, {expiresIn: '1h'});
-            return NextResponse.json({token});
-    } catch (error) {
-        return NextResponse.json({succes:false}, {status:400})
+  //verificar se o usuario existe
+  try {
+    const usuario = await Usuario.findOne({ username });
+    if (!usuario && !(await usuario.comparePassword(password))) {
+      return NextResponse.json({ sucess: false }, { status: 400 });
     }
+
+    //Criar Minha Token de Autorização
+    const token = jwt.sign({ usuarioId: usuario._id }, process.env.JWT_SECRET, {
+      expiresIn: "1h",
+    });
+    return NextResponse.json({ token });
+  } catch (error) {
+    return NextResponse.json({ succes: false }, { status: 400 });
+  }
 }
